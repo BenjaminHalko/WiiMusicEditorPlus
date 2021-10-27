7ac9402246073e8457e59d3b609e41f4716d10b3
BenjaminHalko <benjamin.g.halko@gmail.com>
2021-10-27 15:43:28 -0700

    Updated Build Commands

--- build-linux.sh
+++ build-linux.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-/home/ubuntu/.local/bin/pyinstaller -F -w WiiMusicEditorPlus.py
+/home/ubuntu/.local/bin/pyinstaller -F -w --noconfirm --noupx WiiMusicEditorPlus.py
 rm -r dist/Helper
 cp -r crossplatformhelpers/Linux/Helper dist/Helper
 tar -C dist -cf dist/WiiMusicEditorPlus-Linux.tar.gz WiiMusicEditorPlus Helper
--- build-mac.command
+++ build-mac.command
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd "`dirname "$0"`"
-pyinstaller -F -w --noconfirm WiiMusicEditorPlus.py
+pyinstaller -F -w --noconfirm --noupx WiiMusicEditorPlus.py
 echo "removing old helper"
 rm -r dist/Helper
 echo "copying helper"
