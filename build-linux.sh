67ea1281ccab7fbcc5d66b83235e0782393d070c
BenjaminHalko <benjamin.g.halko@gmail.com>
2021-10-27 16:27:36 -0700

    Update build-linux.sh

--- build-linux.sh
+++ build-linux.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-/home/ubuntu/.local/bin/pyinstaller -F -w --noconfirm --clean WiiMusicEditorPlus.py
+/usr/local/bin/pyinstaller -F -w --noconfirm --clean WiiMusicEditorPlus.py
 rm -r dist/Helper
 cp -r crossplatformhelpers/Linux/Helper dist/Helper
 tar -C dist -cf dist/WiiMusicEditorPlus-Linux.tar.gz WiiMusicEditorPlus Helper
