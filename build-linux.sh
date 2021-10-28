acb805118b7b95fc505a9dac051dd94627bed6ac
BenjaminHalko <benjamin.g.halko@gmail.com>
2021-10-27 19:00:09 -0700

    Update build-linux.sh

--- build-linux.sh
+++ build-linux.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 rm -r __pycache__
+wait 1
 ~/.local/bin/pyinstaller -F -w --noconfirm --clean --noupx WiiMusicEditorPlus.py
 rm -r dist/Helper
 cp -r crossplatformhelpers/Linux/Helper dist/Helper
