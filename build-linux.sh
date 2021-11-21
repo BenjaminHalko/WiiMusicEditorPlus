b3cdd1711d39d1327a89adfe456323db9ac69738
BenjaminHalko <benjamin.g.halko@gmail.com>
2021-11-21 13:10:17 -0800

    Update build-linux.sh

--- build-linux.sh
+++ build-linux.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pyinstaller -F -w --noconfirm -n Program --add-data crossplatformhelpers/Linux/Helper;Helper --icon=Icon/icon.ico WiiMusicEditorPlus.py
+pyinstaller -F -w --noconfirm -n Program --add-data crossplatformhelpers/Linux/Helper:Helper --icon=Icon/icon.ico WiiMusicEditorPlus.py
 mkdir dist/WiiMusicEditorPlus
 mv dist/Program dist/WiiMusicEditorPlus/WiiMusicEditorPlus
 mv dist/WiiMusicEditorPlusProgram dist/WiiMusicEditorPlus/WiiMusicEditorPlus
