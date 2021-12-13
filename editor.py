1364bc78e6f05c3eac53ebf167569c010d125dbe
BenjaminHalko <benjamin.g.halko@gmail.com>
2021-12-12 16:33:36 -0800

    Update editor.py

--- editor.py
+++ editor.py
@@ -304,8 +304,11 @@ def GivePermission(file):
 def Run(command):
 	try:
 		if(type(command) != str): GivePermission(command[0])
-		CREATE_NO_WINDOW = 0x08000000
-		subprocess.run(command,stdout=subprocess.PIPE,stdin=subprocess.PIPE,stderr=subprocess.PIPE,creationflags=CREATE_NO_WINDOW)
+		if(currentSystem == "Windows"):
+			CREATE_NO_WINDOW = 0x08000000
+			subprocess.run(command,stdout=subprocess.PIPE,stdin=subprocess.PIPE,stderr=subprocess.PIPE,creationflags=CREATE_NO_WINDOW)
+		else:
+			subprocess.run(command,stdout=subprocess.PIPE,stdin=subprocess.PIPE,stderr=subprocess.PIPE)
 	except Exception as e:
 		ShowError("Could not execute command:","Command: "+str(command)+"\nError: "+str(e))
 
