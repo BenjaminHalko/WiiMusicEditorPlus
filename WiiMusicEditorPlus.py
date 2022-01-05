27016a43f232deb7864c377d2fb0796fce905699
BenjaminHalko <benjamin.g.halko@gmail.com>
2022-01-05 13:58:19 -0800

    Update WiiMusicEditorPlus.py

--- WiiMusicEditorPlus.py
+++ WiiMusicEditorPlus.py
@@ -524,7 +524,6 @@ class Window(QMainWindow, Ui_MainWindow):
                 env = os.environ
                 if(currentSystem == "Mac"):
                     cmd[0] += "/Contents/MacOS/Dolphin"
-                    print(env["QT_QPA_PLATFORM_PLUGIN_PATH"])
                     env["QT_QPA_PLATFORM_PLUGIN_PATH"] = os.path.dirname(editor.dolphinPath)+'/Contents/MacOS/platforms/'
                     env["QT_DEBUG_PLUGINS"] = 1
                 elif(currentSystem == "Windows"):
