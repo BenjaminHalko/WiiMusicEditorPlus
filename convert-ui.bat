f987f712b73d8dbcd3f1f84ea65d5832d8b85f43
BenjaminHalko <benjamin.g.halko@gmail.com>
2022-01-26 10:34:30 -0800

    Update convert-ui.bat

--- convert-ui.bat
+++ convert-ui.bat
@@ -1,5 +1,6 @@
 @echo off
 
-if [%1]==[] (set name=WiiMusicPlus) else (set name=%~n1)
+set /p namepath=Path of .ui file: 
+for %%f in ("%namepath%") do set name=%%~nf
 
 pyuic5 -o %name%_ui.py %name%.ui
